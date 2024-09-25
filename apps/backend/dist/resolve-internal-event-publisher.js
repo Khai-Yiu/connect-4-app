a1e0c5d3c8eadf83784cb6b0a489e676dd9fe042
Khai-Yiu <khai.soh@journeyone.com.au>
2024-09-26 01:53:46 +0800

    Changed package names and modified configuration files

--- apps/backend/dist/resolve-internal-event-publisher.js
+++
@@ -1,9 +0,0 @@
-const resolveInternalEventPublisher = (subject) => {
-    if (process.env.STAGE === 'DEV') {
-        return (eventDetails) => Promise.resolve(subject.next(eventDetails));
-    }
-    else {
-        throw new Error('Prod not implemented.');
-    }
-};
-export default resolveInternalEventPublisher;
--- apps/backend/dist/utils/to-be-uuid.js
+++
@@ -1,9 +0,0 @@
-const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
-const toBeUuid = function (received) {
-    const { isNot } = this !== null && this !== void 0 ? this : {};
-    return {
-        pass: received.match(UUID_REGEX) !== null,
-        message: () => `${received} is${isNot ? ' a valid' : ' an invalid'} v4 UUID`
-    };
-};
-export default toBeUuid;
